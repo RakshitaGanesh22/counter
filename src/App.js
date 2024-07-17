840edffb5984f9817b866b9415be09c35f816ca6
RakshitaGanesh22 <rakshitaganesh705@gmail.com>
2024-07-17 07:20:01 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useEffect } from 'react';
 function App() {
+  const[usevar,setUse]=useState(0);
+  function HandleValue(event){
+    console.log(event.target.value);
+    let val=event.target.value;
+    if(val==="Increament"){
+      setUse((prev)=>{return prev+1})
+    }
+    else{
+      setUse((prev)=>{return prev-1})
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Counter App</h1>
+        <div>Count: {usevar}</div>
+        <button value="Increament" onClick={HandleValue}>Increament</button>
+        <button value="Decrement" onClick={HandleValue}>Decrement</button>
+</div>
     </div>
   );
 }
